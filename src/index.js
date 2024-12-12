cf7a5e5a967517a74f0ae6e75ed20ae1f9d6b94d
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2024-12-12 16:12:37 +0700

    Install prettier, lint staged, husky

--- src/index.js
+++ src/index.js
@@ -7,33 +7,31 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true })); //Phân tích (parse) dữ liệu được gửi từ các biểu mẫu HTML (form) với phương thức POST hoặc PUT.
+app.use(express.json()); //Phân tích (parse) dữ liệu JSON được gửi từ client, thường qua các API request với header, Chuyển dữ liệu JSON thành một đối tượng JavaScript và lưu trong req.body.
+
 // Cấu hình Handlebars
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs',
-}));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Middleware
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
-// Routes
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/old',(req, res) => {
-  res.render('old');
-})
+// route init
+route(app);
 
 // Lắng nghe cổng
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
